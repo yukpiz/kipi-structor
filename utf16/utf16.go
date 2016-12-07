95469362e8f7e46aa50a123fa13bb2e17b3aa0e5
yukpiz <yukpiz@gmail.com>
2016-12-07 20:14:25 +0900

    Reading full text mode.

--- utf16/utf16.go
+++ utf16/utf16.go
@@ -1,8 +1,10 @@
 package utf16
 
 import (
+	"bytes"
 	"golang.org/x/text/encoding/unicode"
 	"golang.org/x/text/transform"
+	"io/ioutil"
 	"os"
 )
 
@@ -15,4 +17,18 @@ func NewUTF16Scanner(f *os.File) UTF16Scanner {
 	utf16bom := unicode.BOMOverride(win16be.NewDecoder())
 	unicodeReader := transform.NewReader(f, utf16bom)
 	return unicodeReader
+}
+
+func ReadUTF16File(fpath string) ([]byte, error) {
+	raw, err := ioutil.ReadFile(fpath)
+	if err != nil {
+		return nil, err
+	}
+
+	win16be := unicode.UTF16(unicode.BigEndian, unicode.IgnoreBOM)
+	utf16bom := unicode.BOMOverride(win16be.NewDecoder())
+
+	unicodeReader := transform.NewReader(bytes.NewReader(raw), utf16bom)
+	decoded, err := ioutil.ReadAll(unicodeReader)
+	return decoded, err
 }
