7bb68ace6d4f7a4506fd718e48b61e4483a4f41f
yukpiz <yukpiz@gmail.com>
2016-12-07 20:19:39 +0900

    Updated test.

--- utf16/utf16_test.go
+++ utf16/utf16_test.go
@@ -5,8 +5,9 @@ import (
 	"testing"
 )
 
+const fpath string = "/home/yukpiz/.go/extend/src/github.com/yukpiz/kipi-structor/data/itemdb.japan.txt"
+
 func TestNewUTF16Scanner(t *testing.T) {
-	fpath := "/home/yukpiz/.go/extend/src/github.com/yukpiz/kipi-structor/data/itemdb.japan.txt"
 	f, err := os.Open(fpath)
 	if err != nil {
 		t.Errorf("Can not open text file: %s => %s", fpath, err)
@@ -14,5 +15,15 @@ func TestNewUTF16Scanner(t *testing.T) {
 	s := NewUTF16Scanner(f)
 	if s == nil {
 		t.Errorf("Scanner is nil.")
+	}
+}
+
+func TestReadUTF16File(t *testing.T) {
+	b, err := ReadUTF16File(fpath)
+	if err != nil {
+		t.Errorf("Failed to read text file.")
+	}
+	if len(b) == 0 {
+		t.Errorf("Content is empty.")
 	}
 }
